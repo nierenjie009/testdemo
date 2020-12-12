aa72862caa66e737713fb947d6b21ce27f978905
nierenjie009 <nierenjie009@163.com>
2020-12-13 00:17:28 +0800

    修改demo1.js

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@ window.onload =function(){
 
     let box =document.getElementsByClassName('box')[0]
     box.addEventListener('click', () =>{
-       alert("这是测试代码abcd123")
+       alert("这是测试代码abcd12345")
     })
 }
