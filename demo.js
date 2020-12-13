2f429e802e8d7ddc82d78ca845db68fd39334e85
nierenjie009 <nierenjie009@163.com>
2020-12-13 10:48:06 +0800

    haha

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@ window.onload =function(){
 
     let box =document.getElementsByClassName('box')[0]
     box.addEventListener('click', () =>{
-       alert("这是测试代码001")
+       alert("这是测试代码0101")
     })
 }
