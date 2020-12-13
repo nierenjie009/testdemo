f100700e5b9ae1a60a594e2650fbeb8d2dc21e7a
nierenjie009 <nierenjie009@163.com>
2020-12-13 10:54:07 +0800

    hello

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@ window.onload =function(){
 
     let box =document.getElementsByClassName('box')[0]
     box.addEventListener('click', () =>{
-       alert("这是测试代码010221")
+       alert("这是测试代码01025521")
     })
 }
