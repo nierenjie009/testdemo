da2f9d458cede7d1d43413aa6c862a468c730f42
nierenjie009 <nierenjie009@163.com>
2020-12-19 16:05:32 +0800

    aaa

--- demo.js
+++ demo.js
@@ -3,5 +3,6 @@ window.onload =function(){
     let box =document.getElementsByClassName('box')[0]
     box.addEventListener('click', () =>{
        alert("这是测试代码0025")
+       console("这是dev分支的代码")
     })
 }
