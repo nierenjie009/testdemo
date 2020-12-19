c3db3ee5654950b260a436d3a1af4a080b9aa789
nierenjie009 <nierenjie009@163.com>
2020-12-19 19:08:48 +0800

    111

--- demo.js
+++ demo.js
@@ -5,6 +5,6 @@ window.onload =function(){
        alert("这是测试代码abcd1223233")
        alert("这是测试代码0025")
        console("这是master分支的代码")
-       console("这是dev分支的代码123")
+       console("这是的dev分支的代码")
     })
 }
